123091e1b70c768948bc713ed9f983194bc1f5f7
Vitalii Vitriak <vitalii.vitriak@smarttek.solutions>
2025-05-16 14:26:11 +0300

    [IMP] td_contact_sale: change Many2many field for One2many add new model and add new field 'Typical Subclient'

--- todoltd/sale/td_contact_sale/models/res_partner.py
+++ todoltd/sale/td_contact_sale/models/res_partner.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class Partner(models.Model):
@@ -8,9 +9,20 @@ class Partner(models.Model):
         comodel_name='td.res.country.region'
     )
     full_partner_name = fields.Char()
-    sub_client_ids = fields.Many2many(
-        comodel_name='res.partner',
-        relation='res_partner_sub_client_rel',
-        column1='partner_id',
-        column2='sub_client_id',
+    sub_client_rel_ids = fields.One2many(
+        comodel_name='td.res.partner.sub.client.rel',
+        inverse_name='partner_id',
     )
+
+    @api.onchange('sub_client_rel_ids')
+    def _onchange_sub_client_rel_ids(self):
+        for partner in self:
+            typical_len = len(
+                partner.sub_client_rel_ids.filtered(
+                    lambda res: res.is_typical
+                )
+            )
+            if typical_len > 1:
+                raise ValidationError(_(
+                    "This client can have only 1 typical subclient"
+                ))
