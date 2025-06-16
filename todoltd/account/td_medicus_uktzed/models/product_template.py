c9f5b6a57d41faab7cf848b117ed46ee6e6a513d
Vitalii Vitriak <vitalii.vitriak@smarttek.solutions>
2025-06-16 19:21:50 +0300

    [FIX] td_medicus_uktzed: fix comments ( task 10307)

--- todoltd/account/td_medicus_uktzed/models/product_template.py
+++ todoltd/account/td_medicus_uktzed/models/product_template.py
@@ -1,5 +1,5 @@
-from odoo import fields, models
-
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -7,3 +7,8 @@ class ProductTemplate(models.Model):
     td_uktzed_code_id = fields.Many2one(
         comodel_name='td.uktzed'
     )
+
+    @api.constrains('td_uktzed_code_id')
+    def _check_partner(self):
+        if not self.td_uktzed_code_id.selectable:
+            raise ValidationError(_("You can't press this code"))
