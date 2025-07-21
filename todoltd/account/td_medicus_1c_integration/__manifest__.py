886d2a41f7cfb6e9f582db92e79a569e816b2c74
Vitalii Vitriak <vitalii.vitriak@smarttek.solutions>
2025-07-21 11:36:05 +0300

    [FIX] td_medicus_1c_integration: fix comments (task 10870)

--- todoltd/account/td_medicus_1c_integration/__manifest__.py
+++ todoltd/account/td_medicus_1c_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'TD Medicus 1C Integration',
-    "version": '18.0.0.0.11',
+    "version": '18.0.0.0.12',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
