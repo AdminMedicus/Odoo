400f9f939fcaf07cac4be75f15b55ddd0f2efa54
gnezamay <gnezamay@todo.ltd>
2025-07-31 07:55:07 +0000

    [ADD] ata_exchange_v4 add to medicus repo N10887

---
+++ todoltd/exchange/ata_exchange_v4/models/ata_exchange_model_handler.py
@@ -0,0 +1,69 @@
+from odoo import api, models, _
+
+from .ata_exchange_model_handler_mixin import AtaExchangeModelHandlerMixin, RecordHandlerParams
+
+
+class AtaExchangeModelHandler(models.AbstractModel):
+    _name = "ata.exchange.model.handler"
+    _description = "Exchange model handler"
+
+    @api.model
+    def model_handler(self,
+        record_params: RecordHandlerParams) -> models.BaseModel:
+
+        Model = record_params['model']
+        records = self.search_records(record_params)
+        if (not records and record_params['create_record']) or (records and record_params['write_record']):
+            if isinstance(Model, AtaExchangeModelHandlerMixin):
+                vals = Model.ata_exchange_prepare_vals(record_params)
+            else:
+                vals = record_params['data']
+            
+            if vals:
+                if records:
+                    records.write(vals)
+                else:
+                    records = Model.create([vals])
+
+                self.save_matching_data(records, record_params)
+            
+        return records
+
+    @api.model
+    def search_records(self,
+        record_params: RecordHandlerParams) -> models.BaseModel:
+
+        Model = record_params['model']
+        records = Model.browse(None)
+        search_params = record_params['search_params']
+        
+        if (search_domain := search_params['search_domain']):
+            records = Model.search(search_domain)
+
+        if not records and search_params['use_matching_data']:
+            # get matching data
+            key_matching: str = record_params['data'].get(search_params['key_matching_data'], False)
+            matching_id = self.env['ata.exchange.matching.data'].get_matching_data(search_params, key_matching)
+            matching_data: dict = matching_id.matching_data if matching_id else {}
+            
+            # search record in matching data
+            search_id = matching_data.get(record_params['model_name'], False)
+            records = Model.browse(search_id).exists()
+
+        if not records and (search_domain_second := search_params['search_domain_second']):
+            records = Model.search(search_domain_second)
+        
+        return records
+
+    def save_matching_data(self,
+        records: models.BaseModel,
+        record_params: RecordHandlerParams) -> None:
+
+        search_params = record_params['search_params']
+
+        if search_params['use_matching_data']:
+            key_object: str = record_params['data'].get(search_params['key_matching_data'], False)
+            self.env['ata.exchange.matching.data'].save_matching_data(
+                search_params,
+                key_object,
+                {records._name: records[0].id})        
