c055bb96af0dfab43882e90ac385e185cb810ec4
Vitalii Vitriak <vitalii.vitriak@smarttek.solutions>
2025-08-18 14:16:23 +0300

    [FIX] td_medicus_1c_integration: change version to 19

--- todoltd/account/td_medicus_1c_integration/__manifest__.py
+++ todoltd/account/td_medicus_1c_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'TD Medicus 1C Integration',
-    "version": '18.0.0.0.18',
+    "version": '18.0.0.0.19',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
