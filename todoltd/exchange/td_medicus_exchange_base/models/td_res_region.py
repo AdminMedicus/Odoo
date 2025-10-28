f28d0245cd254a9269420641b2454f47caee08f6
gnezamay <gnezamay@todo.ltd>
2025-10-28 08:04:42 +0000

    [ADD] td_medicus_exchange add *_sale, FarmPrihod and other N10887

--- todoltd/exchange/td_medicus_exchange_base/models/td_res_region.py
+++ todoltd/exchange/td_medicus_exchange_base/models/td_res_region.py
@@ -3,9 +3,9 @@ from odoo import api, fields, models
 from odoo.addons.ata_exchange_v4.models.ata_exchange_model_handler_mixin import RecordHandlerParams
 
 
-class TdOneCCategory(models.Model):
-    _name = "td.one_c.category"
-    _inherit = ["td.one_c.category",'ata.exchange.model.handler.mixin']
+class TdResRegion(models.Model):
+    _name = "td.res.country.region"
+    _inherit = ["td.res.country.region",'ata.exchange.model.handler.mixin']
 
     full_name = fields.Char(
         string="Full name",
@@ -14,11 +14,11 @@ class TdOneCCategory(models.Model):
         recursive=True
     )
     
-    @api.depends('name', 'parent_id', 'parent_id.full_name')
+    @api.depends('name', 'parent_region_id', 'parent_region_id.full_name')
     def _compute_full_name(self):
         for record in self:
-            if record.parent_id:
-                record.full_name = f"{record.parent_id.full_name}/{record.name}"
+            if record.parent_region_id:
+                record.full_name = f"{record.parent_region_id.full_name}/{record.name}"
             else:
                 record.full_name = record.name or "" 
     
@@ -31,7 +31,7 @@ class TdOneCCategory(models.Model):
         parent_id = None
         if len(name_list) > 1:
             full_name_parent = "/".join(name_list[:-1])
-            parent_params = record_params.build(self.env, 'td.one_c.category')
+            parent_params = record_params.build(self.env, 'td.res.country.region')
             parent_params.data = {'full_name': full_name_parent}
             parent_params.create_record = True
             parent_params.search_params.search_domain = [
@@ -41,7 +41,7 @@ class TdOneCCategory(models.Model):
         
         vals = {
             "name": name_list[-1],
-            **({"parent_id": parent_id.id} if parent_id else {})
+            **({"parent_region_id": parent_id.id} if parent_id else {})
         }
         
         return vals
