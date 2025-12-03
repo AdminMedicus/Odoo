715d8dd77f3fee513957c56af41158da1298fc74
yhorodniy-todo <y.horodnii@todo.ltd>
2025-12-03 11:17:00 +0200

    [IMP] td_medicus_report: update report and it`s data

--- todoltd/account/td_medicus_report/models/res_partner.py
+++ todoltd/account/td_medicus_report/models/res_partner.py
@@ -13,4 +13,27 @@ class ResPartner(models.Model):
     td_license_issued_by = fields.Char(
         string='License Issued By'
     )
+    td_short_name = fields.Char(
+        string='Short Name',
+        store=True
+    )
+    contact_address_complete = fields.Char(compute='_td_compute_complete_address', store=False)
+
+
+    def _td_compute_complete_address(self):
+        for record in self:
+            record.contact_address_complete = ''
+            if record.zip:
+                record.contact_address_complete += record.zip + ', '
+            if record.country_id:
+                record.contact_address_complete += record.country_id.name + ', '
+            if record.state_id:
+                record.contact_address_complete += record.state_id.name + ', '
+            if record.city:
+                record.contact_address_complete += record.city + ', '
+            if record.street:
+                record.contact_address_complete += record.street + ', '
+            if record.street2:
+                record.contact_address_complete += record.street2 + ', '
+            record.contact_address_complete = record.contact_address_complete.strip().strip(',')
     
