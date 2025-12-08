86ab242c5bb180f361a147217cd0ad5eb1088689
yhorodniy-todo <y.horodnii@todo.ltd>
2025-12-08 09:32:45 +0200

    [IMP] td_medicus_report: update ordering eye lenses report

--- todoltd/account/td_medicus_report/__manifest__.py
+++ todoltd/account/td_medicus_report/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "ToDo Medicus Reports",
     'summary': 'Друковані форми для Medicus',
-    'version': '18.0.0.0.4',
+    'version': '18.0.0.0.5',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
