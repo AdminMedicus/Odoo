55a4467b1c1d186d7a20539fa41ccf20890aae0d
yhorodniy-todo <y.horodnii@todo.ltd>
2025-12-24 16:51:34 +0200

    [IMP] [13712] td_medicus_report: update refund report

--- todoltd/account/td_medicus_report/__manifest__.py
+++ todoltd/account/td_medicus_report/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "ToDo Medicus Reports",
     'summary': 'Друковані форми для Medicus',
-    'version': '18.0.0.0.10',
+    'version': '18.0.0.0.11',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
