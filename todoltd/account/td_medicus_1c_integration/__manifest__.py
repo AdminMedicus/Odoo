41501ba5fb892a0e1dec4bdfd02f191407149698
Vitalii Vitriak <vitalii.vitriak@smarttek.solutions>
2025-12-31 02:52:50 +0200

    [IMP][N11542] td_medicus_1c_integration: add new feautures

--- todoltd/account/td_medicus_1c_integration/__manifest__.py
+++ todoltd/account/td_medicus_1c_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'TD Medicus 1C Integration',
-    "version": '18.0.0.0.30',
+    "version": '18.0.0.0.33',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
