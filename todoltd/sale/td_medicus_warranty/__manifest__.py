91a9d9958e3104000dfe26aa2fc064c570f50d3e
yhorodniy-todo <y.horodnii@todo.ltd>
2026-01-08 17:16:05 +0200

    [IMP] [13900, 13901] td_medicus_warranty: update logic for warranty manual creation, update order lines logic and sale order logic

--- todoltd/sale/td_medicus_warranty/__manifest__.py
+++ todoltd/sale/td_medicus_warranty/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Product Warranty Management",
     'summary': 'Manage product warranties and claims',
-    'version': '18.0.0.0.6',
+    'version': '18.0.0.0.7',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
