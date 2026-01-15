24475a31e024d70e41840613bef06f4e9a532394
yhorodniy-todo <y.horodnii@todo.ltd>
2026-01-15 11:57:48 +0200

    [IMP] [13988] td_medicus_1c_integration: update make invoice wizard

--- todoltd/account/td_medicus_1c_integration/__manifest__.py
+++ todoltd/account/td_medicus_1c_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'TD Medicus 1C Integration',
-    "version": '18.0.0.0.35',
+    "version": '18.0.0.0.36',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
