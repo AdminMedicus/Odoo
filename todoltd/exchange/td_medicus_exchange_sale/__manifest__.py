3fde7ae38b19ddeb0d1aa8345f07d43660be54d0
gnezamay <gnezamay@todo.ltd>
2026-01-19 22:11:46 +0000

    [ADD] add exchange ActTransferToSafeKeeping and ActReturnFromSafeKeeping N10887

--- todoltd/exchange/td_medicus_exchange_sale/__manifest__.py
+++ todoltd/exchange/td_medicus_exchange_sale/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Medicus Exchange (Sale)',
-    'version': '18.0.1.4.10',
+    'version': '18.0.1.4.11',
     'summary': 'Sale/stock module for exchange integration',
     'description': """
         Sale/stock module for exchange integration with Medicus system.
