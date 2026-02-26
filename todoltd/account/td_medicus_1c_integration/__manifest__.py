7e3b6418eb66ff9d8510c9641d020724b528d57a
Igor Kutko <gkm0123@gmail.com>
2026-02-26 16:45:11 +0200

    [FIX] td_medicus_1c_integration: a try to fix a merge conflict

--- todoltd/account/td_medicus_1c_integration/__manifest__.py
+++ todoltd/account/td_medicus_1c_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'TD Medicus 1C Integration',
-    "version": '18.0.0.0.38',
+    "version": '18.0.0.0.39',
     'author': 'ToDo',
     'website': 'https://todo.ltd',
     'license': 'OPL-1',
