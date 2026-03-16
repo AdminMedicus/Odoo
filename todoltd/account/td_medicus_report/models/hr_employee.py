6c86fcf427d8c02acc159ad3a6b09116ae048617
Vitalii Vitriak <vitalii.vitriak@smarttek.solutions>
2026-03-16 06:57:41 +0200

    [IMP][N14940] td_medicus_report: add new logic for task

--- todoltd/account/td_medicus_report/models/hr_employee.py
+++ todoltd/account/td_medicus_report/models/hr_employee.py
@@ -1,4 +1,4 @@
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class HrEmployee(models.Model):
@@ -7,3 +7,21 @@ class HrEmployee(models.Model):
     td_partner_short_name = fields.Char(
         string='Short Name'
     )
+
+    td_driver_license_number = fields.Char(
+        string='Driver License Number',
+        compute='_compute_td_driver_license_number',
+        inverse='_inverse_td_driver_license_number',
+        store=False,
+    )
+
+    def _compute_td_driver_license_number(self):
+        for rec in self:
+            partner = rec.user_id.partner_id if rec.user_id and rec.user_id.partner_id else False
+            rec.td_driver_license_number = partner.td_driver_license_number if partner else False
+
+    def _inverse_td_driver_license_number(self):
+        for rec in self:
+            partner = rec.user_id.partner_id if rec.user_id and rec.user_id.partner_id else False
+            if partner:
+                partner.td_driver_license_number = rec.td_driver_license_number
